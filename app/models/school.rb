dee1045b296607e6e815594fb17d916e16a07350
Johan Higuita <higuitajohan@gmail.com>
2019-01-21 16:47:44 -0500

    Rendering new student form

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,7 +1,11 @@
 class School < ApplicationRecord
-  belongs_to :user
-  has_many :students
+
+	belongs_to :user
+	has_many :students
   # has_many :sessions
   
+  def name_with_initial
+  	this.name
+  end
 
 end
