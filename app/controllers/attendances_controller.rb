594cc8d98d54203a9d728f4b994494019b371319
Johan Higuita <higuitajohan@gmail.com>
2019-02-03 15:05:37 -0500

    Download generic excel file

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -9,16 +9,21 @@ class AttendancesController < ApplicationController
 
 	def download_xls
 		generate_excel
+		send_file(
+			"#{Rails.root}/public/asistencia.xls",
+			filename: "Asistencia_Digital.xls",
+			type: "application/xls"
+			)
 
 	end
 
 	def generate_excel
-		byebug
+		#byebug
 		book = Spreadsheet::Workbook.new
 		sheet1 = book.create_worksheet
 		sheet1.row(0).concat %w{Name Country Acknowlegement}
-		sheet1[1,0] = 'Johan'
-		sheet1[1,1] = 'Colombia'
+		sheet1[1,0] = 'Johan Esteban Higuita'
+		sheet1[1,1] = 'From Colombia'
 		sheet1[1,2] = 'Ruby on Rails'
 
 		book.write "#{Rails.root}/public/asistencia.xls"
