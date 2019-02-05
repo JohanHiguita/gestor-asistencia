ff9710c3c6400c47f574d26a9780e81dbff1a929
Johan Higuita <higuitajohan@gmail.com>
2019-02-04 23:45:55 -0500

    fix routes signin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,9 @@
 class AdminController < ApplicationController
 
+	def index
+		redirect_to facilitadors_path
+	end
+
 
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,26 @@ class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(resource)
 		stored_location_for(resource) ||
-		if resource.is_a?(Admin) 
+		if resource.is_a?(Admin)
+			#byebug 
 			facilitadors_path
+
+		else
+			root_path
 		end
 	end
+
+
+	# def after_sign_out_path_for(resource_or_scope)
+	# 	new_admin_session_path
+	# end
+	# def after_sign_out_path_for(resource_or_scope)
+
+	# 	if resource_or_scope.is_a?(User) 
+	# 		new_user_session_path
+	# 	elsif resource_or_scope.is_a?(Admin)	
+	# 		new_user_admin_path
+	# 	end
+
+	# end
 end
