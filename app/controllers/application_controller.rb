7d396482952603b17115860d88dde70ad6d89a63
Johan Higuita <higuitajohan@gmail.com>
2019-02-04 21:27:00 -0500

    Routes after signin admin and user-facilitador

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,10 @@ class ApplicationController < ActionController::Base
 
 	protect_from_forgery with: :null_session
 
-# 	def after_sign_in_path_for(resource)
-# 		stored_location_for(resource) ||
-# 		if resource.is_a?(Admin) 
-# 			"www.google.com"
-# 		end
-#     #   super
-#     # end
-# end
+	def after_sign_in_path_for(resource)
+		stored_location_for(resource) ||
+		if resource.is_a?(Admin) 
+			facilitadors_path
+		end
+	end
 end
